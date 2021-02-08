62e56602b95eec91dd5a0da38d36a80331e3b5c5
Irina Platonova <crazyangeli@yandex.ru>
2021-02-08 21:17:48 +0300

    add Radio.java

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,67 @@
+package ru.netology.domain;
+
+public class Radio {
+    private int currentNumberStation;
+    private int currentVolume;
+
+    public int getCurrentNumberStation() {
+        return currentNumberStation;
+    }
+
+    public void setCurrentNumberStation(int currentNumberStation) {
+        if (currentNumberStation < 0)
+            return;
+        if (currentNumberStation > 9)
+            return;
+        this.currentNumberStation = currentNumberStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0)
+            return;
+        if (currentVolume > 10)
+            return;
+        this.currentVolume = currentVolume;
+    }
+
+    void numberStationNext() {
+        if (getCurrentNumberStation() == 9) {
+            setCurrentNumberStation(0);
+            return;
+        }
+        setCurrentNumberStation(getCurrentNumberStation() + 1);
+    }
+
+    void numberStationPrev() {
+        if (getCurrentNumberStation() == 0) {
+            setCurrentNumberStation(9);
+            return;
+        }
+        setCurrentNumberStation(getCurrentNumberStation() - 1);
+    }
+
+    void numberStationValue(int CurrentNumberStation) {
+
+        setCurrentNumberStation(CurrentNumberStation);
+    }
+
+    void volumePlus() {
+        if (getCurrentVolume() == 10) {
+            return;
+        }
+        setCurrentVolume(getCurrentVolume() + 1);
+        ;
+    }
+
+    void volumeMinus() {
+        if (getCurrentVolume() == 0) {
+            return;
+        }
+        setCurrentVolume(getCurrentVolume() - 1);
+        ;
+    }
+}
